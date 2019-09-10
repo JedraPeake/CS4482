c8c4c06a6466d9f4fcc7ab0ee342981781879223
JedraPeake <jpeake2@uwo.ca>
2019-09-09 23:19:18 -0400

    add library folders to the git ignore..

---
+++ Tools1/jpeake2Tools1/Assets/Editor/MyPlayerEditor.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(MyPlayer))]
+// [CanEditMultipleObjects]    // Attribute used to make a custom editor support multi-object editing.
+public class MyPlayerEditor : Editor
+{
+	SerializedProperty playerNameProp;
+	SerializedProperty ageProp;
+
+	SerializedProperty pokeballsProp;
+	SerializedProperty incubatorsProp;
+	SerializedProperty incensesProp;
+
+	void OnEnable()
+	{
+		// using strings scares me !!!
+		playerNameProp = serializedObject.FindProperty("playerName");
+		ageProp = serializedObject.FindProperty("age");
+
+		pokeballsProp = serializedObject.FindProperty("pokeballs"); ;
+		incubatorsProp = serializedObject.FindProperty("incubators"); ;
+		incensesProp = serializedObject.FindProperty("incenses"); ;
+	}
+
+	public override void OnInspectorGUI()
+	{
+		serializedObject.Update();
+
+		EditorGUILayout.IntSlider(ageProp, 0, 100);
+
+		EditorGUILayout.LabelField("Items:");
+		int indent = EditorGUI.indentLevel;
+		EditorGUI.indentLevel++;
+
+		MyPlayer myTarget = (MyPlayer)target; 
+		myTarget.pokeballs = EditorGUILayout.IntField("Pokeballs", myTarget.pokeballs);
+		myTarget.incubators = EditorGUILayout.IntField("Incubators", myTarget.incubators);
+		myTarget.incenses = EditorGUILayout.IntField("Incenses", myTarget.incenses);
+
+		EditorGUI.indentLevel = indent;
+
+		EditorGUILayout.LabelField("Items3:");
+
+		serializedObject.ApplyModifiedProperties();
+	}
+
+}
---
+++ Tools1/jpeake2Tools1/Assets/Scripts/MyPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum LureTypes { Normal, Moss, Magnetic, Glacial }
+
+[System.Serializable]
+public class Lure
+{
+	public int count;
+}
+
+public class MyPlayer : MonoBehaviour
+{
+	public int age;
+	public string playerName;   // using string name gives a warning
+
+	public string email = "system@pokemongo.com";
+
+	public int pokeballs;
+	public int incubators;
+	public int incenses;
+	public Lure[] lures;
+
+	//// Start is called before the first frame update
+	//void Start()
+	//{
+
+	//}
+
+	//// Update is called once per frame
+	//void Update()
+	//{
+
+	//}
+
+	//item validation? or count?
+}
