709c8da7f5cabe77b25a2a4fa9b5d66c8961f7be
JedraPeake <jpeake2@uwo.ca>
2019-09-13 01:55:26 -0400

    app1 half way

---
+++ App/jpeake2App1/Assets/Scripts/RestartLevel.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RestartLevel : MonoBehaviour
+{
+	public string tag;
+
+	void OnCollisionEnter(Collision coll)
+	{
+		if (coll.collider.tag == tag)
+			SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+	}
+}
